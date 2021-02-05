5bf318ef7e748c5591abfb5a0f952b30b6b762e5
tirth0 <tirtharajsengupta@gmail.com>
2021-02-05 23:35:32 +0530

    long challenge

---
+++ week4/feblong/divisor.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+using namespace std; 
+
+#define mod 1000000007
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/gcd(a,b)
+#define bits(x) __builtin_popcountll(x)
+#define endl "\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef long long int ll;
+
+
+int main(){
+    int n,ct;
+    cin >> n;
+    ct = 11;
+    while (n%--ct!=0);
+    cout << ct;
+    return 0;
+}
