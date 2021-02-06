0a3724df6170aef6de0988b3e0849f100dc22350
tirth0 <tirtharajsengupta@gmail.com>
2021-02-06 13:37:47 +0530

    day2

--- week4/feblong/maximise.cpp
+++ week4/feblong/maximise.cpp
@@ -14,6 +14,27 @@ using namespace std;
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 typedef long long int ll;
 
+void solve(){
+    ll n; cin >> n;
+    vector<ll> A(n);
+    for (ll i=0;i<n;i++)
+        cin >> A[i];
+    sort(A.begin(),A.end());
+    ll x = 0, y = (A.size()-1)/2, z= A.size()-1;
+    ll sum = A[z] -A[y] + A[y] - A[x] + A[z] - A[x];
+    cout << sum << endl;
+}
+
+
 int main(){
+    int t;cin >> t;
+    while (t>0){
+        t--;
+        solve();
+    }
     return 0;
 }
+
+//1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 5 6 7
+//5-1 + 7-5 + 07-1 = 4 + 1 + 6 = 12
+//1-1 +7-1 + 1-7 = 12
