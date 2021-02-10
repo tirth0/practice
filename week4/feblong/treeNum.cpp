22fa653b81b7b845e0a613bd53942b413cd3f583
tirth0 <tirtharajsengupta@gmail.com>
2021-02-11 00:27:25 +0530

    treeNum

--- week4/feblong/treeNum.cpp
+++ week4/feblong/treeNum.cpp
@@ -5,7 +5,6 @@
 #include<algorithm>
 #include <string> 
 #include <cstring>
-#include<set>
 using namespace std; 
 
 #define mod 1000000007
@@ -16,41 +15,40 @@ using namespace std;
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 typedef long long int ll;
 
-vector<int> chile(1000001,0);
 
 class Graph{
+    
     vector<int> * graph;
+    
 
     public:
 
     Graph(int nodes){
         graph = new vector<int>[nodes+1];
+        
     }
-    // void addEdge(int a,int b){
-    //     graph[a].push_back(b);
-    // }
-    void addEdgeWithoutChildren(int a,int b){
+    void addEdge(int a,int b){
         graph[a].push_back(b);
     }
     void dfs(int v,int w,int n){
         vector<bool> visited(n+1,false);
         ll undone = 0;
         stack<pair<int,int>> s;
-        s.push(pair<int,int>(v,w));
+        s.push(make_pair(v,w));
         while(!s.empty()){
             pair<int,int> node = s.top();
             s.pop();
             visited[node.first] = true;
             vector<int> children = graph[node.first];
-        
-            int childs = chile[node.first];
+            int childs = children.size();
             if (node.second%childs!=0){
                 undone += node.second;
             }
             else{
+                
                 for (auto i : children){
-                    if (!visited[i])
-                        s.push(pair<int,int>(i,node.second/childs));
+                    if (!visited[i] && graph[i].size()!=0)
+                        s.push(make_pair(i,node.second/childs));
                 }
             }
         }
@@ -60,7 +58,6 @@ class Graph{
 };
 
 void solve(Graph g,int n){
-    IOS;
     int v,w;
     cin >> v >> w;
     g.dfs(v,w,n);
@@ -71,15 +68,10 @@ int main(){
     int n;
     IOS;
     cin >> n;
-    vector<int> inp(n+1);
     Graph g(n);
     for (int i=2;i<=n;i++){
         int p;cin >> p;
-        inp[i] = p;
-        chile[p]++;
-    }
-    for (int i=2;i<=n;i++){
-        if (chile[i]!=0) g.addEdgeWithoutChildren(inp[i],i);
+        g.addEdge(p,i);
     }
     int t;
     cin >> t;
