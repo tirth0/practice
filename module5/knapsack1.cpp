b8a93778169263565c93b9427fd5c279ce63e8d1
tirth0 <tirtharajsengupta@gmail.com>
2021-04-26 12:57:54 +0530

    knapsack dp1

---
+++ module5/knapsack1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int knapsack(int wt[],int profit[],int w,int n){
+    if (n==0 || w==0) return 0;
+    if (wt[n]>w)
+        return knapsack(wt,profit,w,n-1);
+    else
+        return max(knapsack(wt,profit,w,n-1), profit[n] + knapsack(wt,profit,w-wt[n],n-1));
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int *wt = new int[n];
+    int *price = new int[n];
+    int w;
+    cin >> w;
+    cout << knapsack(wt,price,w,n);
+    return 0;
+}
